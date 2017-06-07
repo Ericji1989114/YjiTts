02c60ef1029ab3a9a35842be6dd473d1243505f9
Ericji <jiyunshyp@gmail.com>
2017-06-07 16:50:40 +0900

    facebook and twitter setting bug modified

--- YjiTalkToStranger/AppDelegate.swift
+++ YjiTalkToStranger/AppDelegate.swift
@@ -30,8 +30,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
     
-    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
-        return FBSDKApplicationDelegate.sharedInstance().application(application, open: url, sourceApplication: sourceApplication, annotation: annotation)
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        if Twitter.sharedInstance().application(app, open: url, options: options) {
+            return true
+        }
+        let sourceApplication: String? = options[UIApplicationOpenURLOptionsKey.sourceApplication] as? String
+        return FBSDKApplicationDelegate.sharedInstance().application(app, open: url, sourceApplication: sourceApplication, annotation: nil)
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
